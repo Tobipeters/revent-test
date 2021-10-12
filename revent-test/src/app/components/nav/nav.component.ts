e642392ad34163823f7d68c67eed0986671b7a0f
Tobipeters <oluwatobilobapeterschem@gmail.com>
2021-10-12 10:22:48 +0100

    checking for active link and styling

--- revent-test/src/app/components/nav/nav.component.ts
+++ revent-test/src/app/components/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+// import { Router } from '@angular/router'
 
 @Component({
   selector: 'app-nav',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav.component.css']
 })
 export class NavComponent implements OnInit {
+  public currentUrl: string = "";
 
-  constructor() { }
+  constructor(
+    // private route: Router
+  ) { }
 
   ngOnInit(): void {
+    this.currentUrl = window.location.hash;
+    console.log(window.location.hash)
   }
+
+  goTo = (url: string) => {
+    window.location.href = url
+    this.currentUrl = window.location.hash;
+    console.log("checking", this.currentUrl, url)
+  }
+
 
 }
